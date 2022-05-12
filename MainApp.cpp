a92c8e788c2650dacbb9421bd3fa2486a3de310b
Eric Chauvin <echauvin823@gmail.com>
2022-05-12 10:19:36 -0700

    Add files via upload

---
+++ MainApp.cpp
@@ -0,0 +1,156 @@
+// Copyright Eric Chauvin, 2021 - 2022.
+
+
+// This is licensed under the GNU General
+// Public License (GPL).  It is the
+// same license that Linux has.
+// https://www.gnu.org/licenses/gpl-3.0.html
+
+
+#include "../CppBase/StIO.h"
+#include "../CppBase/Str.h"
+// #include "../LinuxApi/SetStack.h"
+#include "../CppBase/Casting.h"
+#include "../CppBase/Threads.h"
+
+#include "../WinApi/Signals.h"
+// #include "../LinuxApi/Signals.h"
+
+
+
+#include "MainApp.h"
+
+
+// extern volatile Int32 gotSigFpe;
+
+/*
+void MainApp::checkSignals( void )
+{
+if( gotSigFpe > 0 )
+  throw "gotSigFpe > 0";
+
+}
+*/
+
+
+
+// int MainApp::mainLoop( int argc, char* argv[] )
+int MainApp::mainLoop( void )
+{
+Int32 delay = 200; // milliseconds.
+const char* outFile =
+               "\\Eric\\Main\\NetSrv\\ExeOut.txt";
+
+try
+{
+StIO::putS( "See output at:" );
+StIO::putS( outFile );
+
+
+// Throws an exception if things are not right.
+BasicTypes::thingsAreRight();
+
+
+mainIO.appendChars(
+            "Programming by Eric Chauvin.\n" );
+
+mainIO.appendChars( "Version date: " );
+mainIO.appendChars( getVersionStr() );
+mainIO.appendChars( "\n\n" );
+
+// For Linux:
+// Int32 stackSize = SetStack::getSize();
+// Str showS( stackSize );
+// mainIO.appendChars( "Stack size: " );
+// mainIO.appendStr( showS );
+// mainIO.appendChars( "\n\n" );
+
+Signals::setupControlCSignal();
+Signals::setupFpeSignal();
+Signals::setupIllegalOpSignal();
+Signals::setupBadMemSignal();
+
+
+testSockets();
+
+
+mainIO.appendChars( "End of main app.\n" );
+
+//             mainIO.readAll( fileName );
+mainIO.writeAll( outFile );
+
+Threads::sleep( delay );
+
+return 0;
+}
+catch( const char* in )
+  {
+  mainIO.appendChars( "Exception in main loop.\n" );
+  mainIO.appendChars( in );
+  mainIO.appendChars( "\n" );
+  mainIO.writeAll( outFile );
+
+  Threads::sleep( delay );
+  return 1;
+  }
+
+catch( ... )
+  {
+  const char* in = "An unknown exception"
+                   " happened in the main loop.\n";
+
+  mainIO.appendChars( in );
+  mainIO.writeAll( outFile );
+
+  Threads::sleep( delay );
+  return 1;
+  }
+}
+
+
+void MainApp::testSockets( void )
+{
+
+CharBuf showBuf;
+
+/*
+Uint64 testSocket = socketsWin.openClient(
+                     "www.durangoherald.com",
+                     "443", showBuf );
+if( testSocket == 0 )
+  {
+  mainIO.appendCharBuf( showBuf );
+  mainIO.appendChars(
+              "openClient returned zero.\n" );
+
+  return;
+  }
+*/
+
+Uint64 testSocket = socketsWin.openServer(
+                            "443", showBuf );
+if( testSocket == 0 )
+  {
+  mainIO.appendCharBuf( showBuf );
+  mainIO.appendChars(
+              "openServer returned zero.\n" );
+
+  return;
+  }
+
+
+SocketCpp newConn = SocketsWin::acceptConnect(
+                         testSocket,
+                         showBuf );
+
+if( newConn == 0 )
+  {
+  mainIO.appendChars( "Did not accept a socket.\n" );
+  }
+
+mainIO.appendChars( "Opened a socket.\n" );
+mainIO.appendCharBuf( showBuf );
+
+socketsWin.closeSocket( testSocket );
+mainIO.appendChars( "Closed test socket.\n" );
+}
---
+++ MainApp.h
@@ -0,0 +1,51 @@
+// Copyright Eric Chauvin 2021 - 2022.
+
+
+// See https://ericinarizona.github.io/
+// For more information.
+
+
+// This is licensed under the GNU General
+// Public License (GPL).  It is the
+// same license that Linux has.
+// https://www.gnu.org/licenses/gpl-3.0.html
+
+
+
+#pragma once
+
+
+// I never use the C preprocessor except to use
+// #include statements.
+
+
+#include "../CppBase/BasicTypes.h"
+#include "../CppBase/FileIO.h"
+#include "../Sockets/SocketsWin.h"
+
+
+
+class MainApp
+  {
+  private:
+  FileIO mainIO;
+
+  // The constructor does WSAStartup() and
+  // the destructor does WSACleanup(). So the
+  // Windows dlls stay going for the life of
+  // this app.  Also statistical and other data
+  // is kept for the life of the app.
+
+  SocketsWin socketsWin;
+
+  public:
+  inline static const char* getVersionStr( void )
+    {
+    return "5/10/2022";
+    }
+
+  // Int32 mainLoop( Int32 argc, char* argv[] );
+  Int32 mainLoop( void );
+  void testSockets( void );
+
+  };
