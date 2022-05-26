4eb04631acdcf2c8e1a1063a82a1dac6f125b521
Eric Chauvin <echauvin823@gmail.com>
2022-05-26 12:37:40 -0700

    Add files via upload

--- MainApp.h
+++ MainApp.h
@@ -43,7 +43,7 @@ class MainApp
   public:
   inline static const char* getVersionStr( void )
     {
-    return "5/21/2022";
+    return "5/24/2022";
     }
 
   // Int32 mainLoop( Int32 argc, char* argv[] );
