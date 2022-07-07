c0ba3017221a1b78fd66400a6ba29b831e50a980
Eric Chauvin <chauvine959@gmail.com>
2022-07-07 13:21:36 -0700

    Add files via upload

--- Handshake.cpp
+++ Handshake.cpp
@@ -9,6 +9,7 @@
 
 
 #include "Handshake.h"
+#include "../CryptoBase/Randomish.h"
 
 
 
@@ -28,46 +29,118 @@ msgAr.setU8( 2, Casting::i32ToU8(
 msgAr.setU8( 3, Casting::i32ToU8(
                                  length ));
 
+// Above this point it is always the same
+// structure.
+
+// Version 3.3 is legacy from TLS 1.2.
+msgAr.setU8( 4, 3 );
+msgAr.setU8( 5, 3 );
+
+// The part for Unix time is not in TLS 1.3.
+// These bytes have to be cryptographically random.
+// These are used later in generating the master
+// secret and keys, etc.
+
+CharBuf randBytes;
+Randomish::makeRandomBytes( randBytes, 32 + 10 );
+
+Int32 index = 6;
+for( Int32 count = 0; count < 32; count++ )
+  {
+  msgAr.setU8( index, randBytes.getU8( count ));
+  index++;
+  }
+
+if( index != 38 )
+  throw "Handshake index != 38.";
+
+msgAr.setU8( 38, 0 ); // Legacy session ID length
+                      // is zero.
 
 /*
-msgAr.setU8( 3,
-    ToSendBuf[9] = 3;   // Version Major
-    ToSendBuf[10] = 3;  // Version Minor
+// Cipher suites listed in the order you want
+// to use first.
 
-    // This part for Unix time is not in TLS 1.3 because there's no point in having it.
-    // But it is here in TLS 1.2.
-    ECTime RightNow = new ECTime();
-    RightNow.SetToNow();
-    ulong UnixTime = RightNow.ToUnixTime();
-    ToSendBuf[11] = (byte)(UnixTime >> 24);
-    ToSendBuf[12] = (byte)(UnixTime >> 16);
-    ToSendBuf[13] = (byte)(UnixTime >> 8);
-    ToSendBuf[14] = (byte)(UnixTime);
+==== Send this in an extension?
 
-    // These bytes have to be cryptographically random.
-    // These are used later in generating the master secret and keys, etc.
-    byte[] RandomBytes = new byte[28];
-    CryptoRand.GetBytes( RandomBytes );
 
-    int Index = 15;
-    for( int Count = 0; Count < 28; Count++ )
-      {
-      ToSendBuf[Index] = RandomBytes[Count];
-      Index++;
-      }
+msgAr.setU8( 39, 0 ); // Length high byte.
+msgAr.setU8( 40, 4 ); // Low byte.
 
-    // Index is 15 + 28 = 43.
-    // StatusString += "Index at the end of Random bytes is: " + Index.ToString() + "\r\n";
-    // Index at the end of Random bytes is: 43
+======
+Signature Algorithm Extension
+          rsa_pkcs1_sha256(0x0401),
+          rsa_pkcs1_sha384(0x0501),
+          rsa_pkcs1_sha512(0x0601),
 
-    ToSendBuf[43] = 0; // Session ID Length is zero. It's not resuming a session here.
+          // ECDSA algorithms
+          ecdsa_secp256r1_sha256(0x0403),
+          ecdsa_secp384r1_sha384(0x0503),
+          ecdsa_secp521r1_sha512(0x0603),
 
-    // A browser sends a lot more cipher suites than this so this is
-    // usually a lot longer.  The algorithm you prefer to use should
-    // be listed first.  So they are listed in order of preference.
-    ToSendBuf[44] = 0; // Cipher Suites Length high byte
-    ToSendBuf[45] = 4; // Cipher Suites Length
-    ToSendBuf[46] = 0;
+          // RSASSA-PSS algorithms with public
+ key OID rsaEncryption
+          rsa_pss_rsae_sha256(0x0804),
+          rsa_pss_rsae_sha384(0x0805),
+          rsa_pss_rsae_sha512(0x0806),
+
+          // EdDSA algorithms
+          ed25519(0x0807),
+          ed448(0x0808),
+
+          // RSASSA-PSS algorithms with public
+ key OID RSASSA-PSS
+          rsa_pss_pss_sha256(0x0809),
+          rsa_pss_pss_sha384(0x080a),
+          rsa_pss_pss_sha512(0x080b),
+
+          // Legacy algorithms
+          rsa_pkcs1_sha1(0x0201),
+          ecdsa_sha1(0x0203),
+
+          // Reserved Code Points
+          obsolete_RESERVED(0x0000..0x0200),
+          dsa_sha1_RESERVED(0x0202),
+          obsolete_RESERVED(0x0204..0x0400),
+          dsa_sha256_RESERVED(0x0402),
+          obsolete_RESERVED(0x0404..0x0500),
+          dsa_sha384_RESERVED(0x0502),
+          obsolete_RESERVED(0x0504..0x0600),
+          dsa_sha512_RESERVED(0x0602),
+          obsolete_RESERVED(0x0604..0x06FF),
+          private_use(0xFE00..0xFFFF),
+          (0xFFFF)
+      } SignatureScheme;
+
+      struct {
+          SignatureScheme supported_signature_algorithms&lt;2..2^16-2&gt;;
+      } SignatureSchemeList;
+
+
+
+ Supported Groups Extension
+
+      enum {
+          unallocated_RESERVED(0x0000),
+
+          // Elliptic Curve Groups (ECDHE)
+          obsolete_RESERVED(0x0001..0x0016),
+          secp256r1(0x0017), secp384r1(0x0018), secp521r1(0x0019),
+          obsolete_RESERVED(0x001A..0x001C),
+          x25519(0x001D), x448(0x001E),
+
+          // Finite Field Groups (DHE)
+          ffdhe2048(0x0100), ffdhe3072(0x0101), ffdhe4096(0x0102),
+          ffdhe6144(0x0103), ffdhe8192(0x0104),
+
+          ffdhe_private_use(0x01FC..0x01FF),
+          ecdhe_private_use(0xFE00..0xFEFF),
+          obsolete_RESERVED(0xFF01..0xFF02),
+          (0xFFFF)
+      } NamedGroup;
+
+
+
     ToSendBuf[47] = 0x35; // TLS_RSA_WITH_AES_256_CBC_SHA    = { 0x00,0x35 };
     ToSendBuf[48] = 0;
     ToSendBuf[49] = 0x3D; // TLS_RSA_WITH_AES_256_CBC_SHA256 = { 0x00,0x3D };
@@ -75,6 +148,8 @@ msgAr.setU8( 3,
     ToSendBuf[51] = 0; // Compression Method is null.
 
     // Extensions go here.
+    // There are always extensions in TLS 1.3.
+
     // On the server side it has to allow unknown
     // extensions to be sent so it can ignore them
     // If they are a reasonable length.
@@ -88,15 +163,6 @@ msgAr.setU8( 3,
          (65535)
       } ExtensionType;
 
-    return CustomerTLS.SendBuffer( ToSendBuf );
+*/
 
-    }
-    catch( Exception Except )
-      {
-      StatusString += "Exception in SendCrudeClientHello()\r\n";
-      StatusString += Except.Message + "\r\n";
-      return false;
-      }
-    }
-  */
 }
--- Handshake.h
+++ Handshake.h
@@ -12,95 +12,153 @@
 #include "../CppBase/CharArray.h"
 
 
+// RFC 8446 - The Transport Layer Security
+// (TLS) Protocol Version 1.3
+
+
+// DH is not ephemeral
+
+// DHE is for ephemeral Diffie-Hellman.
+// DH is not ephemeral.
+
+
 class Handshake
   {
   private:
 
   // The types of handshake messages.
   // These are in the order they have to be sent.
-  // Sent by the server at any time:
-  // The client should ignore it?  In later
-  // versions?
+
+  // This would be like a state machine,
+  // where it is expecting the next state
+  // for the handshake.
+
+  // Sent by the server at any time.
+  // The client should ignore it.  This is not
+  // in later versions.  Reserved.
   static const Uint8 HelloRequest = 0;
 
   static const Uint8 ClientHello = 1;
   static const Uint8 ServerHello = 2;
+
+  // hello_verify_request_RESERVED(3),
+
+  static const Uint8 NewSessionTicket = 4;
+  static const Uint8 EndOfEarlyData = 5;
+
+  // hello_retry_request_RESERVED(6),
+
+  static const Uint8 EncryptedExtensions = 8;
   static const Uint8 Certificate = 11;
   static const Uint8 ServerKeyExchange = 12;
   static const Uint8 CertificateRequest = 13;
+
+  // Not used in 1.3.
   static const Uint8 ServerHelloDone = 14;
   static const Uint8 CertificateVerify = 15;
+
+  // Not used in 1.3.
   static const Uint8 ClientKeyExchange = 16;
+
   static const Uint8 Finished = 20;
 
-/*
-      enum {
-          hello_request(0), client_hello(1), server_hello(2),
-          certificate(11), server_key_exchange (12),
-          certificate_request(13), server_hello_done(14),
-          certificate_verify(15), client_key_exchange(16),
-          finished(20), certificate_url(21), certificate_status(22),
-          (255)
+  // Not used in 1.3.
+  // certificate_url(21)
+  // certificate_status(22)
+  // supplemental_data_RESERVED(23),
 
+  static const Uint8 KeyUpdate = 24;
 
-   enum {
-       hello_request(0), client_hello(1), server_hello(2),
-       certificate(11), server_key_exchange (12),
-       certificate_request(13), server_hello_done(14),
-       certificate_verify(15), client_key_exchange(16),
-       finished(20), certificate_url(21), certificate_status(22),
-       (255)
-   } HandshakeType;
+  // When the handshake is done, you get
+  // the hash of everything.
+  static const Uint8 MessageHash = 254;
 
-*/
   // Null for no compression.
   // CompressionMethod.null
   static const Uint8 CompressionMethod = 0;
 
+/*
+Extension type numbers.
+
+        server_name(0),  RFC 6066
+        max_fragment_length(1), RFC 6066
+        status_request(5),  RFC 6066
+        supported_groups(10), RFC 8422
+        signature_algorithms(13),  RFC 8446
+        use_srtp(14),  RFC 5764
+
+// Heartbeat as in the HeartBleed bug.  It was a
+// bug in OpenSSL.  A buffer over-read.  No
+// bounds check.
+        heartbeat(15),  RFC 6520
+
+        application_layer_protocol_negotiation(16),
+                      RFC 7301
+        signed_certificate_timestamp(18), RFC 6962
+        client_certificate_type(19), RFC 7250
+        server_certificate_type(20), RFC 7250
+        padding(21),  RFC 7685
+        pre_shared_key(41),   RFC 8446
+        early_data(42),       RFC 8446
+
+// You have to always have this in the ClientHello.
+// To say it's TLS 1.3.  0x0304
+        supported_versions(43),  RFC 8446
+
+        cookie(44),    RFC 8446
+        psk_key_exchange_modes(45), RFC 8446
+        certificate_authorities(47), RFC 8446
+        oid_filters(48),          RFC 8446
+        post_handshake_auth(49), RFC 8446
+        signature_algorithms_cert(50), RFC 8446
+        key_share(51),     RFC 8446
+        (65535)
+    } ExtensionType;
+
+
+// The kind of signature you want to get in
+// a certificate.  List them in order of preference.
+
+      enum {
+          // RSASSA-PKCS1-v1_5 algorithms
+          rsa_pkcs1_sha256(0x0401),
+          rsa_pkcs1_sha384(0x0501),
+          rsa_pkcs1_sha512(0x0601),
+
+          // ECDSA algorithms
+          ecdsa_secp256r1_sha256(0x0403),
+          ecdsa_secp384r1_sha384(0x0503),
+          ecdsa_secp521r1_sha512(0x0603),
+
+          // RSASSA-PSS algorithms with public
+          // key OID rsaEncryption
+          rsa_pss_rsae_sha256(0x0804),
+          rsa_pss_rsae_sha384(0x0805),
+          rsa_pss_rsae_sha512(0x0806),
+
+          // EdDSA algorithms
+          ed25519(0x0807),
+          ed448(0x0808),
+
+          // RSASSA-PSS algorithms with public
+          // key OID RSASSA-PSS
+          rsa_pss_pss_sha256(0x0809),
+          rsa_pss_pss_sha384(0x080a),
+          rsa_pss_pss_sha512(0x080b),
+
+          // Legacy algorithms
+          rsa_pkcs1_sha1(0x0201),
+          ecdsa_sha1(0x0203),
+
+          // Reserved Code Points
+          private_use(0xFE00..0xFFFF),
+          (0xFFFF)
+      } SignatureScheme;
+
+
+*/
 
   public:
   void makeClientHello( CharArray& msgAr );
 
   };
-
-
-
-/*
-What state is the handshake in?
-Like a state machine.
-What is the next message it expects to get?
-
-        struct {
-            ProtocolVersion client_version;
-            Random random;
-            SessionID session_id;
-            CipherSuite cipher_suites&lt;2..2^16-1&gt;;
-            CompressionMethod compression_methods&lt;1..2^8-1&gt;;
-        } ClientHello;
-
-It is permitted to include extra data after the
-compression method for forward compatibility.
-
-
-
-
-In later versions this is just 32 bytes of
-random data.  The unix time is deprecated.
-The random data has to be cryptographically
-random.
-
-      struct {
-          uint32 gmt_unix_time;
-          opaque random_bytes[28];
-      } Random;
-
-DH is not ephemeral
-
-DHE denotes ephemeral Diffie-Hellman, where
- the Diffie-Hellman
-   parameters are signed by a DSS or RSA
- certificate, which has been
-   signed by the CA.
-
-
-*/
