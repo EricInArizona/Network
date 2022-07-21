eec2a9acb9428a652415a402b006a927a3bb289b
Eric Chauvin <chauvine959@gmail.com>
2022-07-21 14:34:02 -0700

    Add files via upload

--- Handshake.cpp
+++ Handshake.cpp
@@ -61,6 +61,82 @@ msgAr.setU8( 38, 0 ); // Legacy session ID length
 // Cipher suites listed in the order you want
 // to use first.
 
+// Cipher Suites in Appendix A.5 of RFC 4346.
+// And also in...
+
+TLS_NULL_WITH_NULL_NULL  = { 0x00,0x00 };
+
+CipherSuite TLS_RSA_WITH_NULL_MD5                  = { 0x00,0x01 };
+CipherSuite TLS_RSA_WITH_NULL_SHA                  = { 0x00,0x02 };
+CipherSuite TLS_RSA_WITH_RC4_128_MD5               = { 0x00,0x04 };
+CipherSuite TLS_RSA_WITH_RC4_128_SHA               = { 0x00,0x05 };
+CipherSuite TLS_RSA_WITH_IDEA_CBC_SHA              = { 0x00,0x07 };
+CipherSuite TLS_RSA_WITH_DES_CBC_SHA               = { 0x00,0x09 };
+CipherSuite TLS_RSA_WITH_3DES_EDE_CBC_SHA          = { 0x00,0x0A };
+
+TLS_DH_DSS_WITH_DES_CBC_SHA            = { 0x00,0x0C };
+TLS_DH_DSS_WITH_3DES_EDE_CBC_SHA       = { 0x00,0x0D };
+TLS_DH_RSA_WITH_DES_CBC_SHA            = { 0x00,0x0F };
+TLS_DH_RSA_WITH_3DES_EDE_CBC_SHA       = { 0x00,0x10 };
+TLS_DHE_DSS_WITH_DES_CBC_SHA           = { 0x00,0x12 };
+TLS_DHE_DSS_WITH_3DES_EDE_CBC_SHA      = { 0x00,0x13 };
+TLS_DHE_RSA_WITH_DES_CBC_SHA           = { 0x00,0x15 };
+TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA      = { 0x00,0x16 };
+
+TLS_DH_anon_WITH_RC4_128_MD5           = { 0x00,0x18 };
+TLS_DH_anon_WITH_DES_CBC_SHA           = { 0x00,0x1A };
+TLS_DH_anon_WITH_3DES_EDE_CBC_SHA      = { 0x00,0x1B };
+
+// Don't ever let it do one of these export
+// versions.
+TLS_RSA_EXPORT_WITH_RC4_40_MD5         = { 0x00,0x03 };
+TLS_RSA_EXPORT_WITH_RC2_CBC_40_MD5     = { 0x00,0x06 };
+TLS_RSA_EXPORT_WITH_DES40_CBC_SHA      = { 0x00,0x08 };
+TLS_DH_DSS_EXPORT_WITH_DES40_CBC_SHA   = { 0x00,0x0B };
+TLS_DH_RSA_EXPORT_WITH_DES40_CBC_SHA   = { 0x00,0x0E };
+TLS_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA  = { 0x00,0x11 };
+TLS_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA  = { 0x00,0x14 };
+TLS_DH_anon_EXPORT_WITH_RC4_40_MD5     = { 0x00,0x17 };
+TLS_DH_anon_EXPORT_WITH_DES40_CBC_SHA  = { 0x00,0x19 };
+
+cipher suite values { 0x00, 0x1C } and
+                    { 0x00, 0x1D } are reserved
+ to avoid collision with Fortezza-based
+ cipher suites in SSL 3.
+
+TLS_KRB5_WITH_DES_CBC_SHA           = { 0x00,0x1E }:
+TLS_KRB5_WITH_3DES_EDE_CBC_SHA      = { 0x00,0x1F };
+TLS_KRB5_WITH_RC4_128_SHA           = { 0x00,0x20 };
+TLS_KRB5_WITH_IDEA_CBC_SHA          = { 0x00,0x21 };
+TLS_KRB5_WITH_DES_CBC_MD5           = { 0x00,0x22 };
+TLS_KRB5_WITH_3DES_EDE_CBC_MD5      = { 0x00,0x23 };
+TLS_KRB5_WITH_RC4_128_MD5           = { 0x00,0x24 };
+TLS_KRB5_WITH_IDEA_CBC_MD5          = { 0x00,0x25 };
+
+TLS_KRB5_EXPORT_WITH_DES_CBC_40_SHA    = { 0x00,0x26};
+TLS_KRB5_EXPORT_WITH_RC2_CBC_40_SHA    = { 0x00,0x27};
+TLS_KRB5_EXPORT_WITH_RC4_40_SHA        = { 0x00,0x28};
+TLS_KRB5_EXPORT_WITH_DES_CBC_40_MD5    = { 0x00,0x29};
+TLS_KRB5_EXPORT_WITH_RC2_CBC_40_MD5    = { 0x00,0x2A};
+TLS_KRB5_EXPORT_WITH_RC4_40_MD5        = { 0x00,0x2B};
+
+TLS_RSA_WITH_AES_128_CBC_SHA      = { 0x00, 0x2F };
+TLS_DH_DSS_WITH_AES_128_CBC_SHA   = { 0x00, 0x30 };
+TLS_DH_RSA_WITH_AES_128_CBC_SHA   = { 0x00, 0x31 };
+TLS_DHE_DSS_WITH_AES_128_CBC_SHA  = { 0x00, 0x32 };
+TLS_DHE_RSA_WITH_AES_128_CBC_SHA  = { 0x00, 0x33 };
+TLS_DH_anon_WITH_AES_128_CBC_SHA  = { 0x00, 0x34 };
+
+TLS_RSA_WITH_AES_256_CBC_SHA      = { 0x00, 0x35 };
+TLS_DH_DSS_WITH_AES_256_CBC_SHA   = { 0x00, 0x36 };
+TLS_DH_RSA_WITH_AES_256_CBC_SHA   = { 0x00, 0x37 };
+TLS_DHE_DSS_WITH_AES_256_CBC_SHA  = { 0x00, 0x38 };
+TLS_DHE_RSA_WITH_AES_256_CBC_SHA  = { 0x00, 0x39 };
+TLS_DH_anon_WITH_AES_256_CBC_SHA  = { 0x00, 0x3A };
+
+
+
+
 ==== Send this in an extension?
 
 
